4ed73cd467d9f768e10d25ccce405ff83592e32e
livemehere <rhdxoals@gmail.com>
2023-04-26 03:48:28 +0900

    ✨GoF : Factory 패턴 - private 변수 예제

--- GoF-패턴/Factory-pattern.js
+++ GoF-패턴/Factory-pattern.js
@@ -14,3 +14,26 @@ const jpgImage = createImage("jpg");
 const pngImage = createImage("png");
 
 console.log(jpgImage, pngImage);
+
+/* 프라이빗 변수 문법 이전 방법 */
+
+function createPerson(name) {
+  const properties = {};
+
+  const person = {
+    setName: (name) => {
+      properties.name = name;
+    },
+    getName: () => {
+      return properties.name;
+    },
+  };
+
+  person.setName(name);
+  return person;
+}
+
+const p = createPerson("kong");
+console.log(p.getName()); // kong
+p.setName("Ha");
+console.log(p.getName()); // Ha
