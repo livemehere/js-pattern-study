49c969c7ba874784f13f5281b7d61dc4d2398d18
livemehere <rhdxoals@gmail.com>
2023-06-04 23:29:58 +0900

    ✨renderer, scene 배경, 투명도 설정하기

--- threejs-prac/src/ex2.js
+++ threejs-prac/src/ex2.js
@@ -4,9 +4,14 @@ export default function run(){
     const canvas = document.querySelector('#three-canvas');
     const renderer = new THREE.WebGLRenderer({
         canvas,
-        antialias:true
+        antialias:true,
+        alpha:true // 배경이 투명해지기 때문에, css 컬러와 합성된다.
     });
+
+
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color('blue');
+
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.y = 1;
@@ -26,6 +31,8 @@ export default function run(){
 
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1); // width, height, css 처리까지 해줌 (1 혹은 2로 하는 것이 성능면에서 유리하다)
+        renderer.setClearColor('#00ff00');
+        renderer.setClearAlpha(0.5)
         renderer.render(scene, camera)
 
     }
