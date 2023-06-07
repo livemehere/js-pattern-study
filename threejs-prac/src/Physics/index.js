b8dc09814c58ce6a7c3da2bf4091f51341a6503b
livemehere <rhdxoals@gmail.com>
2023-06-08 02:04:25 +0900

    ✨학습 : 물리엔진 적용하기
    - cannon 과 mesh 연결해서 물건 떨어뜨리기

--- threejs-prac/src/Physics/index.js
+++ threejs-prac/src/Physics/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import * as CANNON from "cannon-es";
 
 export default function run(){
     const scene = new THREE.Scene();
@@ -23,15 +24,56 @@ export default function run(){
 
     /* PlayGround */
 
+    const cannonWorld = new CANNON.World();
+    cannonWorld.gravity.set(0,-9.82,0);
 
+    const groundBody = new CANNON.Body({
+        mass:0,
+        shape:new CANNON.Plane(),
+        position:new CANNON.Vec3(0,0,0)
+    });
+    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+    cannonWorld.addBody(groundBody);
+
+    const boxBody = new CANNON.Body({
+        mass:3,
+        shape:new CANNON.Box(new CANNON.Vec3(0.25,2.5,0.5)),
+        position:new CANNON.Vec3(0,10,0)
+    })
+    cannonWorld.addBody(boxBody);
+
+    const planeGeometry = new THREE.PlaneGeometry(10,10);
+    const planeMaterial = new THREE.MeshPhongMaterial({color:'gray'});
+    const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+    plane.rotation.x = -Math.PI/2;
+    scene.add(plane);
+
+    const boxGeometry = new THREE.BoxGeometry(0.5,5,1);
+    const boxMaterial = new THREE.MeshPhongMaterial({color:'hotpink'});
+    const box = new THREE.Mesh(boxGeometry,boxMaterial);
+    box.position.set(0,0.5,0);
+    scene.add(box);
 
     /* - */
 
+    const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#three-canvas') });
+    renderer.setSize(innerWidth, innerHeight);
+
+    const clock = new THREE.Clock();
     function animate(){
-        requestAnimationFrame(animate);
-        const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#three-canvas') });
-        renderer.setSize(innerWidth, innerHeight);
+        const delta = clock.getDelta();
+        let cannonStep = 1/60;
+        if(delta < 0.01){
+            cannonStep = 1/120;
+        }
+        cannonWorld.step(cannonStep,delta,3);
+        groundBody.position.copy(plane.position);
+        box.position.copy(boxBody.position);
+        box.quaternion.copy(boxBody.quaternion);
+
         renderer.render(scene,camera);
+        requestAnimationFrame(animate);
+
     }
     animate();
 }
